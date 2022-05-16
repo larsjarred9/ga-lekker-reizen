16cc3be190a472da98cec2a0344f5a6da7c4178c
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-16 16:52:45 +0200

    datatablejs & dutch translation

---
+++ assets/js/datatables.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.data-table').DataTable( {
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.12.0/i18n/nl-NL.json"
+        }
+    } );
+} );
