dff42e00298c2f33f8d05361f260f69c683d1994
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-16 16:01:12 +0200

    Navbar chunk

---
+++ assets/include/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">GLR</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Navigatie Aanpassen">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link">Reizen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Verhaal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <a href="login.php" class="btn btn-primary"><i class="fa-solid fa-user"></i> Aanmelden</a>
+            </form>
+        </div>
+    </div>
+</nav>
--- login.php
+++ login.php
@@ -14,30 +14,7 @@ if($_SESSION['user']) {
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">GLR</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Navigatie Aanpassen">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link">Reizen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Verhaal</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <a href="login.php" class="btn btn-primary"><i class="fa-solid fa-user"></i> Aanmelden</a>
-            </form>
-        </div>
-    </div>
-</nav>
+<?php include('assets/include/navbar.php')?>
 
 <header>
     <div class="container">
