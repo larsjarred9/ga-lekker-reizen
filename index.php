7245fc1ea8e964ea9bb0112246a219fb1f210521
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-16 16:53:24 +0200

    Lang NL html

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php session_start() ?>
 <!doctype html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if($_SESSION['user']) {
 }
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
