b44e5e87ef16fd3b03d49c63ad2ed2682fcb01ec
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-16 13:06:39 +0200

    Composer, Medoo & NPM geinstalleerd

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ac437ed27856f3a0c1de7366d4b9d692ab55be72',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ac437ed27856f3a0c1de7366d4b9d692ab55be72',
+            'dev_requirement' => false,
+        ),
+        'catfan/medoo' => array(
+            'pretty_version' => 'v2.1.6',
+            'version' => '2.1.6.0',
+            'type' => 'framework',
+            'install_path' => __DIR__ . '/../catfan/medoo',
+            'aliases' => array(),
+            'reference' => '86ac2b376303f6c3c8e915be9051599b67a8a09e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
