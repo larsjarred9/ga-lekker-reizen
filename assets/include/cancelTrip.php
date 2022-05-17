721d5b4ca5668b5af20d26ddd50bf2418eee7df6
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-17 12:14:31 +0200

    Remove trip

--- assets/include/cancelTrip.php
+++ assets/include/cancelTrip.php
@@ -12,11 +12,11 @@ error_reporting(E_ALL);
 if(isset($_GET["id"])) {
 
     $reservation = $database->get('Reservations', "*", ['user_id' => $_SESSION['user']['id'], 'location_id' => $_GET['id']]);
-    $location = $database->delete("Bookings", ['id' => $reservation['booking_id']]);
+    $reserv = $database->delete("Reservations", ['id' => $reservation['id']]);
 
-    if($location->rowCount() > 0) {
-        $reserv = $database->delete("Reservations", ['id' => $reservation['id']]);
-        if($reserv->rowCount() > 0) {
+    if($reserv->rowCount() > 0) {
+        $location = $database->delete("Bookings", ['id' => $reservation['booking_id']]);
+        if($location->rowCount() > 0) {
             header('location: ../../student/trip.php?id=' . $_GET["id"] . '&success=We hebben je succesvol afgemeld voor deze reis');
         }
         else {
