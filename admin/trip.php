4b67292bad5e201b2e4cbb6c2c8cc69e88b9bf08
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-17 12:30:06 +0200

    (WIP)

--- admin/trip.php
+++ admin/trip.php
@@ -27,17 +27,21 @@ if($_SESSION['user']['role'] != 1) {
         header('location: index.php?error=Reis kon niet gevonden worden');
     }
 
+
+    $registrations = $database->select('Bookings', ['[>]locations' => 'ocation_id'],'*', ['location_id' => $_GET['id']]);
+    var_dump($registrations);
+
     $location = $database->get('Locations', ["id", "title", "location", "type", "begin_date", "end_date", "capacity", "description",],["id" => $_GET['id']]); ?>
     <h2><?= $location['title'] ?></h2>
     <p><?= $location['description'] ?></p>
     <br>
     <div class="d-flex justify-content-between">
-    <p><b>Reis ID:</b> <?= $location['id'] ?></p>
-    <p><b>Locatie:</b> <?= $location['location'] ?></p>
-    <p><b>Type:</b> <?= $location['type'] ?></p>
-    <p><b>Begin Datum:</b> <?= $location['begin_date'] ?></p>
-    <p><b>Eind Datum:</b> <?= $location['end_date'] ?></p>
-    <p><b>Inschrijvingen:</b> X / <?= $location['capacity'] ?></p>
+        <p><b>Reis ID:</b> <?= $location['id'] ?></p>
+        <p><b>Locatie:</b> <?= $location['location'] ?></p>
+        <p><b>Type:</b> <?= $location['type'] ?></p>
+        <p><b>Begin Datum:</b> <?= $location['begin_date'] ?></p>
+        <p><b>Eind Datum:</b> <?= $location['end_date'] ?></p>
+        <p><b>Inschrijvingen:</b> X / <?= $location['capacity'] ?></p>
     </div>
 </section>
 
@@ -48,31 +52,20 @@ if($_SESSION['user']['role'] != 1) {
         <table class="table table-secondary table-striped data-table">
             <thead>
             <tr>
-                <th scope="col">Nummer</th>
+                <th scope="col">Student</th>
                 <th scope="col">Voornaam</th>
                 <th scope="col">Achternaam</th>
                 <th scope="col">Reis</th>
+                <th scope="col">BSN</th>
+                <th scope="col">Additioneel</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry the Bird</td>
-                <td>@fat</td>
-                <td>@twitter</td>
-            </tr>
+                <?php
+//                foreach($registrations as $registration):
+//                    var_dump($registration);
+//                endforeach;
+                ?>
             </tbody>
         </table>
     </div>
--- assets/include/bookTrip.php
+++ assets/include/bookTrip.php
@@ -4,6 +4,26 @@ if($_SESSION['user']['role'] != 0) {
     header('location: ../../login.php');
 }
 
+
+
+// Check of count niet overschreven word
+$location = $database->get('Locations', 'capacity', ['id' => $_POST['id']]);
+
+$count = $database->count('Reservations', ['location_id' => $_POST['id']]);
+
+if($count >= $location) {
+    header('location: ../../student/trip.php?id='.$_POST['id'].'&error=Maximaal aantal inschrijven is al behaald');
+    return false;
+}
+
+// Check of persoon al is aangemeld
+$aangemeld = $database->has('Reservations', ['location_id' => $_GET['id'], 'user_id' => $_SESSION['user']['id']]);
+
+if($aangemeld) {
+    header('location: ../../student/trip.php?id='.$_POST['id'].'&error=Je bent al inschreven voor deze reis');
+    return false;
+}
+
 // Maak booking
 $booking = $database->insert("Bookings", [
     "student_number" => $_POST['student_number'],
