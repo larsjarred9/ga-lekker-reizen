e4d34383d22215e7d017db59c735a29f8de205df
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-17 12:30:02 +0200

    check of aangemeld

--- student/trip.php
+++ student/trip.php
@@ -1,6 +1,11 @@
 <?php require_once('../assets/include/config.php');
+// Check of sessie gezet is
+if(!$_SESSION['user']) {
+    header('location: ../login.php');
+}
+// Check of gebruiker een user is
 if($_SESSION['user']['role'] != 0) {
-    header('location: login.php');
+    header('location: ../login.php');
 }?>
 <!doctype html>
 <html lang="nl">
@@ -46,7 +51,7 @@ if($_SESSION['user']['role'] != 0) {
         <p><b>Type:</b> <?= $location['type'] ?></p>
         <p><b>Begin Datum:</b> <?= $location['begin_date'] ?></p>
         <p><b>Eind Datum:</b> <?= $location['end_date'] ?></p>
-        <p><b>Inschrijvingen:</b> X / <?= $location['capacity'] ?></p>
+        <p><b>Inschrijvingen:</b> <?= $count = $database->count('Reservations', ['location_id' => $_GET['id']]) ?> / <?= $location['capacity'] ?></p>
     </div>
 </section>
 
@@ -54,34 +59,39 @@ if($_SESSION['user']['role'] != 0) {
     <div class="container">
         <h2>Aanmelden</h2>
         <?php
+
+
         // Check of persoon al is aangemeld
         $aangemeld = $database->has('Reservations', ['location_id' => $_GET['id'], 'user_id' => $_SESSION['user']['id']]);
 
-        if(!$aangemeld):?>
-        <p>Vul u gegevens in. Na verzending worden u gegevens gecontrolleerd en ontvangt u een bevestiging in u e-mail inbox.</p>
-        <form action="/assets/include/bookTrip.php" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="id" value="<?= $_GET['id']?>"
-            <label>Voornaam</label>
-            <input value="<?= $_SESSION['user']['first_name'] ?>" readonly class="form-control">
+        if(!$aangemeld):
+            if($count < $location['capacity']):?>
+                <p>Vul u gegevens in. Na verzending worden u gegevens gecontrolleerd en ontvangt u een bevestiging in u e-mail inbox.</p>
+                <form action="/assets/include/bookTrip.php" method="post" enctype="multipart/form-data">
+                    <input type="hidden" name="id" value="<?= $_GET['id']?>"
+                    <label>Voornaam</label>
+                    <input value="<?= $_SESSION['user']['first_name'] ?>" readonly class="form-control">
 
-            <label class="mt-3">Achternaam</label>
-            <input value="<?= $_SESSION['user']['last_name'] ?>" readonly class="form-control">
+                    <label class="mt-3">Achternaam</label>
+                    <input value="<?= $_SESSION['user']['last_name'] ?>" readonly class="form-control">
 
-            <label class="mt-3">Student Nummer</label>
-            <input required name="student_number"min="1" max="99999" type="number" class="form-control">
+                    <label class="mt-3">Student Nummer</label>
+                    <input required name="student_number"min="1" max="99999" type="number" class="form-control">
 
-            <label class="mt-3">BSN Nummer</label>
-            <input required name="bsn" min="1" max="999999999" type="number" class="form-control">
-
-
-            <label class="mt-3">Additionele Informatie</label>
-            <textarea name="remark" class="form-control"></textarea>
-            <p><small>Vul hier gegevens in over bijvoorbeeld alergenen of lichamelijke klachten.</small></p>
+                    <label class="mt-3">BSN Nummer</label>
+                    <input required name="bsn" min="1" max="999999999" type="number" class="form-control">
 
 
-            <button type="submit" name="submit" class="btn btn-primary mt-3">Inschrijven</button>
-        </form>
-        <?php else:?>
+                    <label class="mt-3">Additionele Informatie</label>
+                    <textarea name="remark" class="form-control"></textarea>
+                    <p><small>Vul hier gegevens in over bijvoorbeeld alergenen of lichamelijke klachten.</small></p>
+
+
+                    <button type="submit" name="submit" class="btn btn-primary mt-3">Inschrijven</button>
+                </form>
+            <?php else: ?>
+                <p>Helaas. Alle beschikbare plekken zijn al gereserveerd door medestudenten. Hervers regelmatige deze pagina voor afvallers of boek een andere reis.</p>
+            <?php endif; else:?>
             <p>Je bent al aangemeld voor deze reis. Indien u zich wil afmelden kan dit gemakkelijk via de onderstaande knop doen. Indien u al een betaling heeft verricht moet u contact met ons opnemen.</p>
             <a href="../assets/include/cancelTrip.php?id=<?= $_GET['id']?>" class="btn btn-primary"><i class="fa-solid fa-power-off"></i> Afmelden</a>
         <?php endif;?>
