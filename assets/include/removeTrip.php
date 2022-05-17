0dd65b215ec19dfe500731f1b2eab70b1e6cd9eb
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-17 14:56:32 +0200

    Verwijder locatie

--- assets/include/removeTrip.php
+++ assets/include/removeTrip.php
@@ -1,12 +1,24 @@
 <?php
+
 require_once('config.php');
 if($_SESSION['user']['role'] != 1) {
     header('location: ../../login.php');
 }
 
-
 if(isset($_GET["id"])) {
+    // krijg booking_ids
+    $reservations = $database->select('Reservations', 'booking_id', ['location_id' => $_GET["id"]]);
 
+    // Verwijder reservaties
+    $database->delete('Reservations', ['location_id' => $_GET["id"]]);
+
+    // Verwijder bookingen
+    foreach($reservations as $reservation) {
+        echo $reservation;
+        $booking = $database->delete("Bookings", ['id' => $reservation]);
+    }
+
+    // Verwijder locatie
     $location = $database->delete("Locations", ['id' => $_GET["id"]]);
 
     if($location->rowCount() > 0) {
