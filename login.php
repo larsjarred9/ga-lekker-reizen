d82405037c1e4e81fe9411b9179dc449c11302d7
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-17 11:20:06 +0200

    contact chunk

--- login.php
+++ login.php
@@ -44,20 +44,7 @@ if($_SESSION['user']) {
     <img src="assets/images/forrest.jpg" alt="" class="img-fluid w-100">
 </section>
 
-<section class="container col-xl-10 col-xxl-8">
-    <div class="row align-items-center">
-        <div class="col-lg-7 text-center text-lg-start">
-            <h2 class="">Contact</h2>
-            <p class="col-lg-10">Ga Lekker Reizen is onderdeel van Grafisch Lyceum Rotterdam. Onze enthousiaste medewerkers zoeken dagelijks naar de leukste en goedkoopste vakanties van diverse touroperators. Alle reizen boek je direct met studentenkorting!</p>
-            <a href="mailto:84644@glr.nl" target="_blank" class="btn btn-primary"><i class="fa-solid fa-envelope"></i> E-Mail</a>
-            <a href="tel:0637449583" target="_blank" class="btn btn-primary"><i class="fa-solid fa-mobile"></i> Bellen</a>
-            <a href="https://wa.me/0637449583" target="_blank" class="btn btn-primary"><i class="fa-brands fa-whatsapp"></i> Whatsapp</a>
-        </div>
-        <div class="col-md-10 mx-auto col-lg-5">
-            <img src="assets/images/airplane.jpg" class="img-fluid">
-        </div>
-    </div>
-</section>
+<?php include('assets/include/contact.php')?>
 
 <?php include('assets/include/footer.php')?>
 
