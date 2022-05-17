d60ca7ba37a4b1cca359720e06692462175eb770
Lars Jarred Speetjens <lars@speetjens.net>
2022-05-17 09:37:08 +0200

    Create trip

---
+++ assets/include/createTrip.php
@@ -0,0 +1,42 @@
+<?php
+require_once('config.php');
+
+
+if(isset($_FILES["image"])) {
+
+    $target_dir = "../images/uploads/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    // Alleen JPG & JPEG
+    if ($imageFileType != "jpg" && $imageFileType != "jpeg") {
+        header('location: ../../admin/createTrip.php?error=Alleen JPG en JPEG afbeeldingen mogen worden geupload');
+    } else {
+        // Toevoegen aan database
+        $database->insert("Locations", [
+            "title" => $_POST['title'],
+            "location" => $_POST['location'],
+            "type" => $_POST['type'],
+            "begin_date" => $_POST['begin_date'],
+            "end_date" => $_POST['end_date'],
+            "capacity" => $_POST['capacity'],
+            "description" => $_POST['description'],
+        ]);
+
+        // Target maken
+        $new_target = $target_dir . $database->id().'.jpg';
+
+        // Bestand uploaden
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $new_target)) {
+            header('location: ../../admin/index.php?success=Reis is successvol toegevoegd');
+        } else {
+            header('location: ../../admin/createTrip.php?error=Er iets verkeerd gegaan met het upslaan van de afbeelding');
+        }
+    }
+} else {
+    header('location: ../../admin/createTrip.php?error=Er iets verkeerd gegaan met het upslaan van de afbeelding');
+}
+
+
+
+?>
